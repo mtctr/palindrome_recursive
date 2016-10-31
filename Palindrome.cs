5c0a6f78a264646cf7bec8e1117d0f6b59588c04
mtctr <mateustargino95@gmail.com>
2016-10-31 00:18:34 -0300

    Code for the palindrome recursive method

---
+++ Palindrome.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Palindrome {
+
+        //checa se é palindromo recursivamente
+        static bool palindrome(String s) {
+            if (s.Length == 0 || s.Length == 1) {     //caso base: palava possui tamanho 0 ou 1
+                return true;
+            }
+            else if (s.First().Equals(s.Last())){     //verifica se o primeiro e o ultimo caractere são iguais
+                s = s.Substring(1, s.Length - 2);     //retira o primeiro e o ultimo caractere da string
+                //Console.WriteLine(s);                 //debug
+
+                return palindrome(s);                //chamada recursiva
+            }         
+                                    
+            return false;                        
+                                      
+        }
+
+        static void Main(string[] args){
+            String s;
+
+            s = Console.ReadLine();
+
+            if (palindrome(s)) {
+                Console.WriteLine("é palindromo");
+            }
+            else {
+                Console.WriteLine("não é palindromo");
+            }
+                      
+        }
+
+    }
+    
+}
